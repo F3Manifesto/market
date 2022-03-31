b9554dc511380feec72f580257425a3e909bcec2
James Lin <wonyy1988@outlook.com>
2022-03-31 18:45:41 -0400

    feat: add gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/product/[id]/[rarity]/[isAuction]":["static\u002Fchunks\u002Fpages\u002Fproduct\u002F[id]\u002F[rarity]\u002F[isAuction].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fproduct\u002F[id]\u002F[rarity]\u002F[isAuction]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
