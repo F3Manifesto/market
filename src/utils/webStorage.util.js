0e38d56d83f574b080308445b2b9c5ffcd3b0a8e
jupiteruwdev <harutoinoue8@gmail.com>
2022-04-29 09:03:24 -0400

    market sign in issue fixed.

--- src/utils/webStorage.util.js
+++ src/utils/webStorage.util.js
@@ -1,10 +1,11 @@
-export const APPROVAL_FOR_ALL = 'APPROVAL_FOR_ALL';
+export const F3M_MARKET_APPROVAL_FOR_ALL = "F3M_MARKET_APPROVAL_FOR_ALL";
 
 export const getApprovalStatus = () => {
-  const isApproved = localStorage.getItem(APPROVAL_FOR_ALL) || 'false';
+  const isApproved =
+    localStorage.getItem(F3M_MARKET_APPROVAL_FOR_ALL) || "false";
   return JSON.parse(isApproved);
 };
 
 export const setApprovalStatus = (isApproved) => {
-  localStorage.setItem(APPROVAL_FOR_ALL, JSON.stringify(isApproved));
+  localStorage.setItem(F3M_MARKET_APPROVAL_FOR_ALL, JSON.stringify(isApproved));
 };
